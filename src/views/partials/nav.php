bf6ca9bfe89cb94f3a9dfd1a337ee3ec65625687
thongabcxyz <thongcv12vn@gmail.com>
2024-05-05 19:40:31 +0700

    resolve conflict

--- src/views/partials/nav.php
+++ src/views/partials/nav.php
@@ -8,23 +8,34 @@ use app\models\UserRole;
 use app\utils\DataHelper;
 
 ?>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="/home"><i class="fa-solid fa-phone"></i></i> Phone Retail Store</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="/home"><i class="fa-solid fa-phone"></i></i> Phone Retail Store</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/home"><i class="fa-solid fa-house"></i>
+                        Trang chủ</a>
+                </li>
+                <?php if ($isAuthenticated && $sessionUser->getRole() == UserRole::ADMIN): ?>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/home"><i class="fa-solid fa-house"></i>
-                            Trang chủ</a>
+                        <a class="nav-link active" href="/admin"><i class="fa-solid fa-user-tie"></i> Admin</a>
                     </li>
+                <?php endif; ?>
                     <?php if ($isAuthenticated && $sessionUser->getRole() == UserRole::ADMIN): ?>
                         <li class="nav-item">
                             <a class="nav-link active" href="/admin"><i class="fa-solid fa-user-tie"></i> Admin</a>
+                        </li>
+                    <?php endif; ?>
+                    <?php if ($isAuthenticated): ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/product"><i
+                                        class="fa-solid fa-mobile"></i> Sản Phẩm</a>
                         </li>
                     <?php endif; ?>
                     <?php if ($isAuthenticated): ?>
@@ -38,7 +49,13 @@ use app\utils\DataHelper;
                             <a class="nav-link active" aria-current="page" href="/customer"><i class="fa-solid fa-book"></i> Quản Lí Khách Hàng</a>
                         </li>
                     <?php endif; ?>
+                    <?php if ($isAuthenticated): ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/statistics"><i class="fas fa-pen-alt"></i> Thống kê doanh thu</a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
+            
 
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <?php if (!$isAuthenticated): ?>
